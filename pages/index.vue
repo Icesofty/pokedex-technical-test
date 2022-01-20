997939378e7048802e68854f4a136ed83d25fd07
Florian <argaudflorian@gmail.com>
2022-01-20 19:09:37 +0100

    feat: limited to the first 151 pokemons

--- pages/index.vue
+++ pages/index.vue
@@ -23,23 +23,16 @@
         <NuxtImg :src="pokemon.sprite" :alt="pokemon.name" class="w-20 h-20" />
         <p class="text-center text-xs">{{ pokemon.name }}</p>
       </NuxtLink>
-      <div v-if="loading">Loading...</div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      offset: 0,
-      loading: false,
-    }
-  },
   async asyncData({ $axios }) {
     const pokemons = []
     const pokemonsList = await $axios.$get(
-      'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0'
+      'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
     )
     const next = pokemonsList.next
 
@@ -68,43 +61,6 @@ export default {
     if (this.$store.state.localStorage.pokemons.length === 0) {
       this.$store.commit('localStorage/updateList', this.pokemons)
     }
-  },
-  mounted() {
-    this.getNextPokemons()
-  },
-  methods: {
-    async fetchPokemons(pokemonsList) {
-      this.loading = true
-      const newPokemons = []
-      for (const pokemon of pokemonsList.results) {
-        const { name, sprites, abilities, stats, id } = await this.$axios.$get(
-          pokemon.url
-        )
-        newPokemons.push({
-          name,
-          sprite: sprites.front_default,
-          abilities,
-          stats,
-          id,
-        })
-      }
-      this.pokemons = [...this.pokemons, ...newPokemons]
-      this.$store.commit('localStorage/updateList', newPokemons)
-      this.loading = false
-    },
-    getNextPokemons() {
-      window.onscroll = () => {
-        let bottomOfWindow =
-          document.documentElement.scrollTop + window.innerHeight ===
-          document.documentElement.offsetHeight
-        if (bottomOfWindow && this.next && !this.loading) {
-          this.$axios.$get(this.next).then((response) => {
-            this.next = response.next || false
-            this.fetchPokemons(response)
-          })
-        }
-      }
-    },
   },
 }
 </script>
